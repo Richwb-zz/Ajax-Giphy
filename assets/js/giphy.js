db127c211e3d41e53531f3d6b47034a6cca051a7
unknown <richwbates@gmail.com>
2017-08-23 10:18:51 -0400

    buttons load in their own div as do images images using fixed_height url

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,25 +1,34 @@
-var topics = ["Looney Tunes", "Scuba", "Troll", "Evil Cats", "Laser", "Coding"];
+// Holds topics
+var topics = ["Looney Tunes", "Scuba", "Trending", "Evil Cats", "Laser", "Coding"];
 
+// Loops through topics and creates buttons
 $.each(topics, function(index, value){
-	$(".container").append("<button class='btn btn-primary'>" + value + "</button>");
+	$("#buttons").append("<button class='btn btn-primary'>" + value + "</button>");
 });
 
-
+// on button click get gifs with that topic name
 $(document).on("click", ".btn", function(){
 	loadGif($(this).text());
 });
 
+// Switches the static image with the Gif and vice versa
+$(document).on("click", ".gif", function(){
+	var newDataUrl =  $(this).attr("src");
+	$(this).attr("src", $(this).attr("data-url"));
+	$(this).attr("data-url", newDataUrl); 
+});
 
-
+// Giphy api call
 function loadGif(topic){
 	$.ajax({
-	   url: "https://api.giphy.com/v1/gifs/search?q=" + topic + "&limit=10&api_key=dc6zaTOxFJmzC",
+	   url: "https://api.giphy.com/v1/gifs/search?q=" + topic + "&rating=g&rating=pg&rating=pg-13&limit=10&api_key=dc6zaTOxFJmzC",
 	   method: "GET"
-	 }).done(function(response) {
-		
+	}).done(function(response) {
+		console.log(response);
+	 	// Loops through api and gets the gifs and static image and put them in a image tag
 		$.each(response.data, function(index, gif){
 
-	   	$(".container").append("<img src='" + gif.images.original_still.url + "'' class='gif' data-url='" + gif.images.original.url + "'></img>");
+	   	$("#images").append("<img src='" + gif.images.fixed_height_still.url + "'' class='gif' data-url='" + gif.images.fixed_height.url + "'></img>");
 
 		});
 
